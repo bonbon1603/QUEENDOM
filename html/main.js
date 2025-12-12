5a8c3ad9ab190a862ff4bdd40f6a79c8e92c4350
bonbon1603 <bongbon668@gmail.com>
2025-12-12 23:51:28 +0700

    Delete html directory

--- html/main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("translate-btn").onclick = async () => {
-    const text = document.getElementById("text-input").value;
-
-    const words = normalizeSentence(text);
-
-    console.log("Processed words:", words);
-
-    await playVideoSequence(words);
-};
